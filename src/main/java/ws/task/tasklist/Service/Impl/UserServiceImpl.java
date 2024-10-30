85f2355f3d92cc7b64ebe6f2e1735efb38c1a681
Alexander Tsyganov <tsyganovalexandr98@gmail.com>
2024-10-30 20:44:26 +1000

    Create userMapper, service, task and user repositoryImpl

--- src/main/java/ws/task/tasklist/Service/Impl/UserServiceImpl.java
+++ src/main/java/ws/task/tasklist/Service/Impl/UserServiceImpl.java
@@ -1,46 +1,72 @@
 package ws.task.tasklist.Service.Impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ws.task.tasklist.Entity.Role;
 import ws.task.tasklist.Entity.User;
+import ws.task.tasklist.Exception.ResourceNotFoundException;
+import ws.task.tasklist.Repository.UserRepository;
 import ws.task.tasklist.Service.UserService;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    private final UserService userService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     @Transactional(readOnly = true)
     public User getById(Long id) {
-        return null;
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
     @Override
     @Transactional(readOnly = true)
     public User getByUsername(String username) {
-        return null;
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
     @Override
+    @Transactional
     public User update(User user) {
-        return null;
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.update(user);
+        return user;
     }
 
     @Override
+    @Transactional
     public User create(User user) {
-        return null;
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            throw new IllegalStateException("User already exists");
+        }
+        if (user.getPassword().equals(user.getPasswordConfirmation())) {
+            throw new IllegalStateException("Password confirmation don't match");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.create(user);
+        Set<Role> roles = Set.of(Role.ROLE_USER);
+        userRepository.insertUserRole(user.getId(), Role.ROLE_USER);
+        user.setRoles(roles);
+        return user;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public boolean isTaskOwner(Long userId, Long taskId) {
-        return false;
+        return userRepository.isTaskOwner(userId, taskId);
     }
 
     @Override
+    @Transactional
     public void delete(Long userId) {
-
+        userRepository.delete(userId);
     }
 }
--- src/main/java/ws/task/tasklist/config/ApplicationConfig.java
+++ src/main/java/ws/task/tasklist/config/ApplicationConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -20,7 +21,7 @@ import ws.task.tasklist.security.JwtTokenProvider;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
+@RequiredArgsConstructor(onConstructor_ = @__(@Lazy))
 public class ApplicationConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
